962f76b1011d08edf69c901e3e00585666b10e21
FinchFeng <329650437@qq.com>
2018-10-29 17:16:45 +0800

    Initial Commit

---
+++ iPv6Changer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iPv6Changer
+//
+//  Created by 冯奕琦 on 2018/10/29.
+//  Copyright © 2018年 冯奕琦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
